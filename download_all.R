a2e48f3baeecd6101f6adbf02c3ad0e86575b93c
Danapit <pitdana@yahoo.co.uk>
2022-11-21 19:27:30 +0100

    more comments

--- download_all.R
+++ download_all.R
@@ -6,23 +6,25 @@ library(dplyr) # set of tools for data manipulation, see details at https://dply
 #=============== 1. SEARCH (by PROJECT) and download citations =================
 # Documentation of PANGAEA search: https://wiki.pangaea.de/wiki/PANGAEA_search
 # Website: https://www.pangaea.de/?q=project:label:PAGES_C-PEAT
-# search with pg_search: maximum = 500 records (set with count, continue with offset)
-# 2022-11: There are datasets PUBLISHED
+# search with pg_search: maximum = 500 records (set with count, continue with offset to add additional record beyond 500) 
+# 2022-11: There are 875 datasets PUBLISHED
 PAGES <- pg_search("project:label:PAGES_C-PEAT", count = 500)
 
 # Download all dataset citations:
 # rbind function: joins 2 or more dataframes
 PAGES <- rbind(PAGES, pg_search("project:label:PAGES_C-PEAT", count = 500, offset = 500))
+#create a column with full citation
 PAGES$fullcitation <- paste0(PAGES$citation, ". PANGAEA, https://doi.org/", PAGES$doi)
 
-# write table as txt file
+# write citations as txt file
 write.table(sort(PAGES$fullcitation), file="citations_pages.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t", na = "")
 
 #=============== 2. SETTING PANGAEAR CACHE ===================
+# why is this relevant? pg_data automatically writes the .tab file to the cache folder, so it is nice to have it "at hand", See 3. GET DATA
 # check the location of the cache path of pangaear
 pg_cache$cache_path_get()
 
-# create a folder for download
+# create a folder for download - a warning comes if the directory already exists
 getwd()
 dir.create(path=paste0(getwd(),"/Downloads"))
 folderpath <- (paste0(getwd(),"/Downloads"))
@@ -38,7 +40,7 @@ pg_cache$cache_path_set(full_path = folderpath)
 Joey_core12 <- pg_data(doi="10.1594/PANGAEA.890405")
 Joey_core12 <- Joey_core12[[1]][["data"]]
 
-# create a folder for download
+# create a folder for download of edited data tables - a warning comes if the directory already exists
 getwd()
 dir.create(path=paste0(getwd(),"/Data"))
 datapath <- paste0(getwd(),"/Data/")
@@ -48,8 +50,10 @@ datapath <- paste0(getwd(),"/Data/")
 write.table(Joey_core12, file=paste0(datapath,"Joey_core12.txt"), row.names = FALSE, quote = FALSE, sep = "\t", na = "")
 
 #=============== 4. FILTER SEARCH RESULTS =================
+# in this scenario, we are only interested in "geochemistry" PAGES_C-PEAT datasets from a limited area in Nothern Sweden
+# 
 # restrict the search by geographical coordinates with an attribute bbox=c(minlon, minlat, maxlon, maxlat)
-# datasets in northern Sweden
+# pay attention to the coordinate sequence and correct signs ("-"/negative for South and West)
 PAGES_Sweden <- pg_search("project:label:PAGES_C-PEAT", count = 500, bbox=c(17.7, 67.7, 21, 69))
 
 # filter only datasets with "Geochemistry" in title (column citation)
@@ -58,7 +62,7 @@ PAGES_Sweden <- pg_search("project:label:PAGES_C-PEAT", count = 500, bbox=c(17.7
 PAGES_Sweden <- filter(PAGES_Sweden, grepl("Geochemistry", citation))
 
 #=============== 5. GET MULTIPLE DATA =================
-# combine data into a single data frame
+# combine data from the previous search into a single data frame
 
 # initiate a data frame
 PAGES_Sweden_data <- data.frame()
